7d388568c4bf300ff28dbe3158f3af082757c710
carlos-nevesaraujo <45833371+carlos-nevesaraujo@users.noreply.github.com>
2024-09-03 22:36:23 -0300

    Add files via upload

---
+++ script/login.v1.js
@@ -0,0 +1,32 @@
+function validateForm() {
+    var isValid = true;
+
+    // Clear previous errors
+    $(".form-control").removeClass("is-invalid");
+
+    var email = $("#email").val();
+
+    // Basic validation checks
+    if (!email) {
+        isValid = false;
+    }
+
+    if(email.indexOf("sharepro") < 0)
+    {
+        isValid = false;
+        alert("Acesso negado!")
+    }
+
+  
+    if (isValid) {
+        // Simulate authentication and generate an access token (in real use, you'd authenticate and obtain a token)
+        var accessToken = email; // Replace this with actual token
+
+        // Redirect to Alexa's redirect URL with the access token
+        var redirectUri = "https://pitangui.amazon.com/spa/skill/account-linking-status.html?vendorId=MNMBMHN0IMTVR";
+        window.location.href = redirectUri + "&access_token=" + encodeURIComponent(accessToken);
+    }
+
+    return false; // Prevent form submission to allow redirection
+}
+
